f7a077c957ca6c20be63cf43fcad75b467c9070f
SkyzohKey <skyzohkey@protonmail.ch>
2017-02-20 10:24:28 +0100

    Add linting step to circle.yml

--- misc/build-linux.sh
+++ misc/build-linux.sh
@@ -5,6 +5,15 @@ function install_deps {
   sudo add-apt-repository -y ppa:rebuntu16/glade-3.19+-trusty
   sudo apt-get update
   sudo apt-get install -y build-essential vala-0.30 valac libgtk-3-dev libjson-glib-dev libsoup2.4-dev
+
+  # Ruby is needed to lint code.
+  sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  curl -sSL https://get.rvm.io | bash -s stable
+  source ~/.rvm/scripts/rvm
+  rvm install 2.4.0
+  rvm use 2.4.0 --default
+  ruby -v
 }
 
 function build {
